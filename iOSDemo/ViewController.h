61c5f73a26b69c1a6efc0bdb9b03ad647939abc3
蔡万鸿 <caiwanhong@koolearn.com>
2019-04-04 10:32:12 +0800

    Initial Commit

---
+++ iOSDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSDemo
+//
+//  Created by wanhong cai on 2019/4/4.
+//  Copyright © 2019 Koolearn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSDemo
+//
+//  Created by wanhong cai on 2019/4/4.
+//  Copyright © 2019 Koolearn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
